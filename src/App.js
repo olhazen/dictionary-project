75daac30392c69463afcd3408eaf24859270d1f7
olhazen <107193525+olhazen@users.noreply.github.com>
2022-10-10 14:21:15 +0100

    Added/edited the logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+       <img src={logo} className="App-logo img-fluid" alt="logo" /> 
       </header>
       Hello
     </div>
